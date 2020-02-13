2aba9608343631314ac5bf007c6b3553c94e2864
Jocelyn Beedie <jellonator00@gmail.com>
2020-02-12 22:08:41 -0500

    Tweaked trail

--- Object/Player/Player.gd
+++ Object/Player/Player.gd
@@ -28,6 +28,9 @@ func _physics_process(delta):
 		ivector = ivector.normalized()
 	if ivector.length() > 1e-3:
 		node_smoke.emitting = true
+		var lvec = ivector.rotated(-rotation)
+		part_smoke.direction = Vector3(lvec.x, lvec.y, 0)
+		part_fire.direction = part_smoke.direction
 	else:
 		node_smoke.emitting = false
 	var target_velocity := MAX_SPEED * ivector
@@ -47,25 +50,7 @@ func _physics_process(delta):
 		if lookvector.length_squared() > 1e-2:
 			self.rotation = lerp_angle(self.rotation, lookvector.angle(), delta * 10)
 		do_shoot = Input.is_action_pressed("action_shoot_nonmouse")
-	var prev_pos := global_position
 	velocity = move_and_slide(velocity)
-	var post_pos := global_position
-	if prev_pos == post_pos:
-		part_smoke.emission_shape = ParticlesMaterial.EMISSION_SHAPE_POINT
-		node_smoke.position = part_smoke_offset
-		part_fire.emission_shape = ParticlesMaterial.EMISSION_SHAPE_POINT
-		node_fire.position = part_fire_offset
-	else:
-		part_smoke.emission_shape = ParticlesMaterial.EMISSION_SHAPE_BOX
-		part_smoke.emission_box_extents.x = prev_pos.distance_to(post_pos)
-		node_smoke.global_position = global_transform.xform(part_smoke_offset) +\
-			(prev_pos + post_pos) / 2 - global_position
-		node_smoke.global_rotation = (post_pos - prev_pos).angle()
-		part_fire.emission_shape = ParticlesMaterial.EMISSION_SHAPE_BOX
-		part_fire.emission_box_extents.x = prev_pos.distance_to(post_pos)
-		node_fire.global_position = global_transform.xform(part_fire_offset) +\
-			(prev_pos + post_pos) / 2 - global_position
-		node_fire.global_rotation = (post_pos - prev_pos).angle()
 	var brender = get_tree().get_nodes_in_group("bullet_renderer")[0]
 	if do_shoot:
 		shot_timer -= delta * SHOTS_PER_SECOND
